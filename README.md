7d3d512eb9f853cfdf9a828c4d4399acbe38ebc6
karlag12 <38440109+karlag12@users.noreply.github.com>
2018-04-16 15:53:03 -0400

    code for hello world
    adding the about section

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hello_world
+
+
+
+
+## about section 
+# karla garcia
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  
+//
+//  Created by karla garcia on 4/16/18.
+//
+
+#include <stdio.h>
+# include <isostream>
+usning namespace std:
+
+int main (){
+    cout<<"hello world!"<<endl;
+    return 0
+}
